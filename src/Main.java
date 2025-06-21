8c8df653e9bae6f0c59d2ec6bdab2a8b2d548d0d
sharat kumar J <160093079+sharatkumarjogula@users.noreply.github.com>
2025-06-21 16:37:50 +0530

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("the sum of 3 numbers are : ");
+        int num1 =45;
+        int num2=34;
+        int num3=77;
+        int sum=num1+num2+num3;
+        System.out.println(sum);
+
+    }
+}
